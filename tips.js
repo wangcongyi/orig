09181897b5a7da0af34d00cfbf43a26a424c6e13
wangcongyi <360805958@qq.com>
2016-03-20 12:44:03 +0800

    add switch->obj

--- tips.js
+++ tips.js
@@ -60,3 +60,69 @@ $(btn1).click(function () {
     }
 
 });
+
+
+//    var a = 1;
+//
+//
+//    var color = 'red';
+//
+//
+//    var colorObj = {
+//        'black': printBlackBackground,
+//        'red': printRedBackground,
+//        'blue': printBlueBackground,
+//        'green': printGreenBackground,
+//        'yellow': printYellowBackground
+//    };
+//
+var data = {
+    status: '2'
+};
+
+var fn = {
+    "1": a1,
+    "2": a2,
+    "3": a3
+};
+if(data.status in fn){
+    $.isFunction(fn[data.status]) && fn[data.status]()
+    //  fn[data.status]()
+}
+
+//
+//    if (color in colorObj) {
+//        colorObj[color]();
+//    }
+
+
+function a1() {
+    console.log(111)
+}
+function a2() {
+    console.log(2221)
+}
+function a3() {
+    console.log(1333)
+}
+function a4() {
+    console.log(1444)
+}
+
+/*
+ var fncList = {
+ a: function () {
+ console.log(1)
+ },
+ b: function () {
+ console.log(2)
+ }
+ };
+
+
+ $(document).on('click', '[data-action]', function () {
+ var data = $(this).data('action');
+ var action = fncList[data];
+ $.isFunction(action) && action();
+ })
+ */
