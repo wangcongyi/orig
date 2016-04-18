937e7a79feae2dc99e7ff822f21f8a71402b68d1
wangcongyi <360805958@qq.com>
2016-04-18 23:14:22 +0800

    add javascript design

--- tips.js
+++ tips.js
@@ -1,65 +1,58 @@
-
 // 获取伪类样式ֵ
 
-var color = window.getComputedStyle(document.querySelector('.element'), ':before').getPropertyValue('color');
-
-
-
-
-
-
-var a = 10;
-var b;
-a == 10 && fn1() & fn2();
-
-//  a == undefined || (b = 234);
-//  console.log(b);
-function fn1() {
-    console.log(1)
-}
-function fn2() {
-    console.log(2)
-}
-
-
-
-
-function addEvent(obj, ev, fn) {
-    obj['bind' + ev] = obj['bind' + ev] || {};
-    obj['bind' + ev]['bind' + fn] = obj['bind' + ev]['bind' + fn] || function () {
-            fn.call(obj)
-        };
-
-    obj.attachEvent('on'+ev,obj['bind' + ev]['bind' + fn])
-}
-function removeEvent(obj,ev,fn){
-    if(obj['bind' + ev]['bind' + fn] ){
-        obj.detachEvent('on'+ev,obj['bind' + ev]['bind' + fn])
-    }
-}
-
-var num = 1;
-$(btn2).click(function () {
-    num == 8 && (num = 0);
-    $(Ul).animate({
-        left: -450 * num
-    }, 1000);
-    num++;
-});
-$(btn1).click(function () {
-    if(num==1){
-        $(Ul).animate({
-            left:-450*7
-        },1000);
-        num = 8
-    }else{
-        $(Ul).animate({
-            left:Ul.offsetLeft+450
-        },1000);
-        num --;
-    }
-
-});
+//var color = window.getComputedStyle(document.querySelector('.element'), ':before').getPropertyValue('color');
+//
+//
+//var a = 10;
+//var b;
+//a == 10 && fn1() & fn2();
+//
+////  a == undefined || (b = 234);
+////  console.log(b);
+//function fn1() {
+//    console.log(1)
+//}
+//function fn2() {
+//    console.log(2)
+//}
+//
+//
+//function addEvent(obj, ev, fn) {
+//    obj['bind' + ev] = obj['bind' + ev] || {};
+//    obj['bind' + ev]['bind' + fn] = obj['bind' + ev]['bind' + fn] || function () {
+//            fn.call(obj)
+//        };
+//
+//    obj.attachEvent('on' + ev, obj['bind' + ev]['bind' + fn])
+//}
+//function removeEvent(obj, ev, fn) {
+//    if (obj['bind' + ev]['bind' + fn]) {
+//        obj.detachEvent('on' + ev, obj['bind' + ev]['bind' + fn])
+//    }
+//}
+//
+//var num = 1;
+//$(btn2).click(function () {
+//    num == 8 && (num = 0);
+//    $(Ul).animate({
+//        left: -450 * num
+//    }, 1000);
+//    num++;
+//});
+//$(btn1).click(function () {
+//    if (num == 1) {
+//        $(Ul).animate({
+//            left: -450 * 7
+//        }, 1000);
+//        num = 8
+//    } else {
+//        $(Ul).animate({
+//            left: Ul.offsetLeft + 450
+//        }, 1000);
+//        num--;
+//    }
+//
+//});
 
 
 //    var a = 1;
@@ -76,19 +69,19 @@ $(btn1).click(function () {
 //        'yellow': printYellowBackground
 //    };
 //
-var data = {
-    status: '2'
-};
-
-var fn = {
-    "1": a1,
-    "2": a2,
-    "3": a3
-};
-if(data.status in fn){
-    $.isFunction(fn[data.status]) && fn[data.status]()
-    //  fn[data.status]()
-}
+//var data = {
+//    status: '2'
+//};
+//
+//var fn = {
+//    "1": a1,
+//    "2": a2,
+//    "3": a3
+//};
+//if (data.status in fn) {
+//    $.isFunction(fn[data.status]) && fn[data.status]()
+//    //  fn[data.status]()
+//}
 
 //
 //    if (color in colorObj) {
@@ -96,18 +89,18 @@ if(data.status in fn){
 //    }
 
 
-function a1() {
-    console.log(111)
-}
-function a2() {
-    console.log(2221)
-}
-function a3() {
-    console.log(1333)
-}
-function a4() {
-    console.log(1444)
-}
+//function a1() {
+//    console.log(111)
+//}
+//function a2() {
+//    console.log(2221)
+//}
+//function a3() {
+//    console.log(1333)
+//}
+//function a4() {
+//    console.log(1444)
+//}
 
 /*
  var fncList = {
@@ -143,22 +136,15 @@ function a4() {
 //b = obj && obj.a && obj.a.b && obj.a.b.c;
 
 
-
 //   QQ在线咨询~~~~其实真简单    一个链接+qq号就OK了~~~~
 //   http://wpa.qq.com/msgrd?v=3&uin=  “QQ号”  &site=qq&menu=yes
 
 
-
-
-
-
-
-
 /*
-*    attr & prop
-*
-*
-* */
+ *    attr & prop
+ *
+ *
+ * */
 //var str0 = '<div class="reading"><div class="re-text">相关阅读</div><div class="re-ul"><li><a uuid="C7033408C56000015CF883D0D800B6A0" href="sfsdf" target="_blank" class="reader">fsdf</a></li><li><a uuid="C7033408C57000019CBC1B20A1A5AFB0" href="sdfsdf" target="_blank" class="reader">fsdf</a></li><li><a uuid="C7033408C5700001E6311E2019C088A0" href="fsd" target="_blank" class="reader">fsdfsd</a></li></div></div>';
 //
 //console.log(getUuid(str0,"uuid1"));
@@ -180,9 +166,9 @@ function a4() {
 //};
 
 /*
-*    获取 字符串中的数字  不使用正则
-*
-* */
+ *    获取 字符串中的数字  不使用正则
+ *
+ * */
 //var str = 'sdaddf444sfsfdsf8485sdfsdfdsfds18526dfdsfdsfdsf2dfsdf58';
 //  [444,8485,18526,2,58]
 //var s;
@@ -202,7 +188,7 @@ function a4() {
 
 
 /**
- * @description
+ * @description  闭包返回定时器 再次调用
  * @param {Function} fn     传人的回调函数
  * @param {Number}   times  次数
  * @param {Number}   delay  间隔时间
@@ -225,3 +211,39 @@ function a4() {
 //
 //var a = f(alert, 6, 1000);
 //a('你好');
+
+/**
+ *  @description    javascript 设计模式 工厂模式  稍作修改 有可能UI都是相同的所以把UI也抽出来 后台传递的data参数
+ * @param type
+ * @param content
+ * @returns {*}
+ * @constructor
+ */
+//var F = function (type, content) {
+//    return this[type](content)
+//};
+//F.prototype = {
+//    Java: function (content) {
+//        this.UI(content, 'red');
+//    },
+//    JavaScript: function (content) {
+//        this.UI(content, 'green')
+//    },
+//    UI: function (content, color) {
+//        this.content = content;
+//        (function (content) {
+//            var div = document.createElement('div');
+//            div.innerHTML = content;
+//            div.style.color = color;
+//            document.querySelector('#id').appendChild(div)
+//        })(content)
+//    }
+//};
+//var data = [
+//    {type: 'Java', content: 'Thisi sisd Java'},
+//    {type: 'JavaScript', content: 'Thisi sisd javascsfeprsdsd'}
+//];
+//var i = 0;
+//for (; i < data.length; i++) {
+//    new F(data[i].type, data[i].content)
+//}
