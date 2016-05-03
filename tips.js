7293fd49242932531027fe501d98433750c2f98b
wangcongyi <360805958@qq.com>
2016-05-03 15:48:55 +0800

    add status des

--- tips.js
+++ tips.js
@@ -266,3 +266,50 @@
 //  self.animate({width: 500}, 500, 'linear')
 // }, 1000)
 //})
+
+/**
+ * @description    javascript 状态模式  状态变成私有变量 返回一个对象接口 可以对状态修改或者调用
+ * @returns {{change: (Action.changeState|Function), goes: (Action.goes|Function)}}
+ * @constructor
+ */
+//var MarryState = function () {
+//    var _currentState = {},
+//        states = {
+//            jump: function () {
+//                console.log('jump')
+//            },
+//            move: function () {
+//                console.log('move')
+//            },
+//            shoot: function () {
+//                console.log('shoot')
+//            },
+//            squat: function () {
+//                console.log('squat')
+//            }
+//        };
+//    var Action = {
+//        changeState: function () {
+//            var arg = arguments;
+//            if (arg.length) {
+//                for (var i = 0; i < arg.length; i++) {
+//                    _currentState[arg[i]] = true
+//                }
+//            }
+//            return this
+//        },
+//        goes: function () {
+//            console.log('changeOnce');
+//            for (var i in _currentState) {
+//                states[i] && states[i]()
+//            }
+//            return this
+//        }
+//    };
+//    return {
+//        change: Action.changeState,
+//        goes: Action.goes
+//    }
+//};
+//var m = new MarryState();
+//m.change('jump').goes().change('shoot').goes();
