548759b97dac58531d0816e8966208a298238ff7
王聪奕 <360805958@qq.com>
2017-12-19 19:24:03 +0800

    折角边框样式
    折角边框样式

--- tips.js
+++ tips.js
@@ -738,3 +738,13 @@
 //     })
 // }
 
+///////////////////////////////////////
+//    折角边框样式
+//    background-image: linear-gradient(to right, #DCE3E6 .3rem, transparent .3rem), linear-gradient(to right, #DCE3E6 .3rem, transparent .3rem), linear-gradient(to bottom, #DCE3E6 .3rem, transparent .3rem), linear-gradient(to bottom, #DCE3E6 .3rem, transparent .3rem);
+//    background-size: 100% 1px, 100% 1px, 1px 100%, 1px 100%;
+//    background-position: -.15rem 0, -.15rem 100%, 0 -.15rem, 100% -.15rem;
+//    background-repeat: repeat-x, repeat-x, repeat-y, repeat-y;
+///////////////////////////
+
+
+
