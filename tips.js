12cd8d7bb0b0cc68c9e3ec83eb49a6d9e6a9116c
王聪奕 <360805958@qq.com>
2018-02-07 20:14:58 +0800

    webpack postcss-cssnext and postcss-nested
    webpack postcss cssnext and nested

--- tips.js
+++ tips.js
@@ -644,6 +644,7 @@
 //
 ////////////////////////////////////////////////////
 // 2017-11-14 使用webpack 3.X 版本 和 postcss
+// 2018-2-7 增加 postcss-nested,postcss-cssnext （ cssnext 插件已经包含 autoprefixer 可以在配置文件中删除）
 //  webpack.config.js
 // var ex = require('extract-text-webpack-plugin');
 // module.exports = {
@@ -665,7 +666,9 @@
  //               loader:'postcss-loader',options:{
 //                  plugins: loader => [
 //                    require('postcss-simple-vars')(),
-//                    require('autoprefixer')(),
+//                    require('postcss-cssnext')(),
+//                    require('postcss-nested')(), 
+//                    //require('autoprefixer')(),
 //                    require('cssnano')()
 //                  ]
 //                }
