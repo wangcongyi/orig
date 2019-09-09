40c1f8a92139310a9c25e3ecfed0c8378814fdb8
王聪奕 <360805958@qq.com>
2019-09-09 18:06:34 +0800

    react hooks
    react hooks

--- react-tips.js
+++ react-tips.js
@@ -512,4 +512,292 @@ const Hello = () => {
 
 export default Hello
 
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+//     React Hooks
+import React, { createContext, useContext, useEffect, useState, useRef, useReducer } from 'react'
+import axios from 'axios'
+import './css/variables.css'
+import './App.css'
+
+
+const O = () => {
+  const [name, setName] = useState({ f: '', l: '' })
+  return (
+    <div>
+      <form action="">
+        <input type="text" value={name.f} onChange={e => setName({ ...name, f: e.target.value })} />
+        <input type="text" value={name.l} onChange={e => setName({ ...name, l: e.target.value })} />
+        <h3>{name.f}</h3>
+        <h3>{name.l}</h3>
+      </form>
+    </div>
+  )
+}
+
+const A = () => {
+  const [items, setItems] = useState([])
+  const addItem = () => {
+    setItems([...items, { id: items.length, value: Math.random() * 10 + 1 }])
+  }
+
+  return (
+    <>
+      <button onClick={addItem}>Add number</button>
+      <ul>
+        {items.map(item => <li key={item.id}>{item.value}</li>)}
+      </ul>
+    </>
+  )
+}
+
+const E = () => {
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+  const logPosition = e => {
+    setX(e.clientX)
+    setY(e.clientY)
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', logPosition)
+    return () => window.removeEventListener('mousemove', logPosition)
+  }, [])
+
+  return (
+    <div>
+      X:{x} - Y:{y}
+    </div>
+  )
+}
+
+const T = () => {
+  const [count, setCount] = useState(0)
+  const tick = () => {
+    setCount(c => c + 1)
+  }
+
+  useEffect(() => {
+    const timer = setInterval(tick, 1000)
+    return () => clearInterval(timer)
+  }, [])
+
+  return (
+    <div>
+      {count}
+    </div>
+  )
+}
+
+const F = () => {
+  const [posts, setPost] = useState({})
+  const [id, setId] = useState(1)
+  const [fromClick, setClick] = useState(1)
+
+  const handleClick = () => {
+    setClick(id)
+  }
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${fromClick}`)
+      .then(res => {
+        setPost(res.data)
+        console.log(res)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [fromClick])
+
+  return (
+    <>
+      <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>Fetch data</button>
+      <div>{posts.title}</div>
+    </>
+  )
+}
+
+const Username = React.createContext()
+const C = () => {
+  return (
+    <div>
+      <h3>Pass the context </h3>
+      <C1 />
+    </div>
+  )
+}
+const C1 = () => {
+  const u = useContext(Username)
+  return <div>get the context{u}</div>
+}
+
+
+const initialState = {
+  f: 0,
+  s: 10,
+
+}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case '+':
+      return { f: state.f + action.value }
+    case '-':
+      return { f: state.f - action.value }
+    case 'reset':
+      return initialState
+    default:
+      return state
+
+  }
+}
+const R = () => {
+  const [count, dispatch] = useReducer(reducer, initialState)
+  return (
+    <div>
+      <div>Count: {count.f}</div>
+      <button onClick={() => dispatch({ value: 1, type: '+' })}>++</button>
+      <button onClick={() => dispatch({ value: 1, type: '-' })}>--</button>
+      <button onClick={() => dispatch({ value: 5, type: '+' })}>+5</button>
+      <button onClick={() => dispatch({ value: 5, type: '-' })}>-5</button>
+      <button onClick={() => dispatch({ value: 1, type: 'reset' })}>reset</button>
+    </div>
+  )
+}
+
+
+const m = 0
+const mR = (state, action) => {
+  switch (action) {
+    case '+':
+      return state + 1
+    case '-':
+      return state - 1
+    case 'reset':
+      return m
+    default:
+      return state
+  }
+}
+const M = () => {
+  const [c1, d1] = useReducer(mR, m)
+  const [c2, d2] = useReducer(mR, m)
+
+
+  return (
+    <>
+      <div>
+        <div>Count: {c1}</div>
+        <button onClick={() => d1('+')}>++</button>
+        <button onClick={() => d1('-')}>--</button>
+        <button onClick={() => d1('reset')}>reset</button>
+      </div>
+      <div>
+        <div>Count: {c2}</div>
+        <button onClick={() => d2('+')}>++</button>
+        <button onClick={() => d2('-')}>--</button>
+        <button onClick={() => d2('reset')}>reset</button>
+      </div>
+    </>
+  )
+}
+
+
+const Rcc = React.createContext()
+const RC1 = () => {
+  const { countState, countDispatch } = useContext(Rcc)
+  return (
+    <div>
+      <h4>Component A {countState}</h4>
+      <button onClick={() => countDispatch('+')}>++</button>
+      <button onClick={() => countDispatch('-')}>--</button>
+      <button onClick={() => countDispatch('reset')}>reset</button>
+    </div>
+  )
+}
+const RC2 = () => {
+  const cc = useContext(Rcc)
+  return (
+    <div>
+      <h4>Component B</h4>
+      <button onClick={() => cc.countDispatch('+')}>++</button>
+      <button onClick={() => cc.countDispatch('-')}>--</button>
+      <button onClick={() => cc.countDispatch('reset')}>reset</button>
+    </div>
+  )
+}
+const RC3 = () => {
+  const cc = useContext(Rcc)
+  return (
+    <div>
+      <h4>Component C</h4>
+      <button onClick={() => cc.countDispatch('+')}>++</button>
+      <button onClick={() => cc.countDispatch('-')}>--</button>
+      <button onClick={() => cc.countDispatch('reset')}>reset</button>
+    </div>
+  )
+}
+const RC = () => (
+  <>
+    <RC1 />
+    <RC2 />
+    <RC3 />
+  </>
+)
+
+
+const Rfetch = () => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+  const [post, setPost] = useState({})
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      .then(res => {
+        setLoading(false)
+        setPost(res.data)
+        setError('')
+      })
+      .catch(err => {
+        setLoading(false)
+        setPost({})
+        setError('Something went wrong!'+err)
+      })
+  }, [])
+
+  return (
+    <div>
+      {loading ? 'loading' : post.title}
+      {error ? error : null}
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <Rfetch />
+    </div>
+  )
+}
+
+
+// const App = () => {
+//   const [count, dispatch] = useReducer(mR, m)
+//
+//   return (
+//     <Rcc.Provider
+//       value={{ countState: count, countDispatch: dispatch }}
+//     >
+//       <div className="App">
+//         Count: {count}
+//         <RC />
+//       </div>
+//     </Rcc.Provider>
+//   )
+// }
+
+
+export default App
 
